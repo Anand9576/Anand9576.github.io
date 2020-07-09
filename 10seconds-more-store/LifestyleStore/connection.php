14f1ee29ffce8529672f8ab20d42d42df9a5283c
Anand Kumar <anandrajadarsh9576@gmail.com>
2020-07-09 21:53:19 +0530

    updated

--- 10seconds-more-store/LifestyleStore/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server = "sql202.epizy.com";
-$username = "epiz_26217438";
-$password = "jaS38OC8be1ckb";
-$dbname = "epiz_26217438_anandkumar";
-
-$con=mysqli_connect($server, $username, $password, $dbname) or die(mysqli_error($con));
-?>
